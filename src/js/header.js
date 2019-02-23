51016604a1f46a7cd257c813ee08f3e44246660a
tangqiuyong <1595322725@qq.com>
2019-02-24 02:15:32 +0800

    add a file .gtiigonre

---
+++ src/js/header.js
@@ -0,0 +1,32 @@
+$(function () {
+    // 地区事件=========开始
+    $('.hangzou').on('click', function () {
+        $this = $(this);
+        $('.hangzou').find('.city').addClass('show');
+    });
+
+    $('.city-cha').on('click', function (e) {
+        e.stopPropagation();
+        $('.hangzou .city').removeClass('show');
+    });
+
+    var $li = $('.shang-ul li');
+    var $tap = $('.tap-xia ul')
+    $li.on('click', function () {
+        $(this).addClass('active').siblings('li').removeClass('active');
+        $tap.eq($(this).index()).css('display', 'block').siblings('ul').css('display', 'none');
+    });
+
+    // 地区事件=========结束
+
+    //登录事件=======开始
+
+    $('.topnavul-li1').on('mouseenter', function () {
+        $(this).addClass('active');
+        console.log($('li.topnavul-li1'))
+    });
+    $('.topnavul-li1').on('mouseleave', function () {
+        $(this).removeClass('active');
+    });
+
+}) 
