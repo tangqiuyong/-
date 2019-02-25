3eefa6e74e5a5bf3ea60cacc339a36fd4fa6e2b1
wangsenlin <wangsenlin@idongjia.cn>
2019-02-26 00:15:09 +0800

    修改

--- src/js/header.js
+++ src/js/header.js
@@ -1,57 +1,92 @@
-$(function () {
-    // 地区事件=========开始
-    $('.hangzou').on('click', function () {
-        $this = $(this);
-        $('.hangzou').find('.city').addClass('show');
-    });
+$(function() {
+  // 地区事件=========开始
+  $(".hangzou").on("click", function() {
+    $this = $(this);
+    $(".hangzou")
+      .find(".city")
+      .addClass("show");
+  });
 
-    $('.city-cha').on('click', function (e) {
-        e.stopPropagation();
-        $('.hangzou .city').removeClass('show');
-    });
+  $(".city-cha").on("click", function(e) {
+    e.stopPropagation();
+    $(".hangzou .city").removeClass("show");
+  });
 
-    var $li = $('.shang-ul li');
-    var $tap = $('.tap-xia ul')
-    $li.on('click', function () {
-        $(this).addClass('active').siblings('li').removeClass('active');
-        $tap.eq($(this).index()).css('display', 'block').siblings('ul').css('display', 'none');
-    });
+  var $li = $(".shang-ul li");
+  var $tap = $(".tap-xia ul");
+  $li.on("click", function() {
+    $(this)
+      .addClass("active")
+      .siblings("li")
+      .removeClass("active");
+    $tap
+      .eq($(this).index())
+      .css("display", "block")
+      .siblings("ul")
+      .css("display", "none");
+  });
 
-    // 地区事件=========结束
+  // 地区事件=========结束
 
-    //登录事件=======开始
+  //登录事件=======开始
 
-    $('.topnavul-li1').on('mouseenter', function () {
-        $(this).addClass('active');
-        console.log($('li.topnavul-li1'))
-    });
-    $('.topnavul-li1').on('mouseleave', function () {
-        $(this).removeClass('active');
-    });
+  $(".topnavul-li1").on("mouseenter", function() {
+    $(this).addClass("active");
+    console.log($("li.topnavul-li1"));
+  });
+  $(".topnavul-li1").on("mouseleave", function() {
+    $(this).removeClass("active");
+  });
 
+  //   左边  导航栏
 
-
-
-
-    //   左边  导航栏
-
-$('navinner-ul .navinner-li').hover(function(){
-
-    $('.fenleinav').slideDown('500',function(){
-        $('.fenleinav').show();
-    });
-    
-    },function(){
-        $('.fenleinav').slideUp ('500') ,function(){
-        $('.fenleinav').hide(); 
+  $("navinner-ul .navinner-li").hover(
+    function() {
+      $(".fenleinav").slideDown("500", function() {
+        $(".fenleinav").show();
+      });
+    },
+    function() {
+      $(".fenleinav").slideUp("500"),
+        function() {
+          $(".fenleinav").hide();
+        };
     }
-    
-    });
+  );
 
-    $('.fenleinav p').hover(function(){
-        $this = $(this);
-        $(this).addClass('active');
-    },function(){
-        $(this).removeClass('active');
-    })
-}) 
+  $(".fenleinav p").hover(
+    function() {
+      $this = $(this);
+      $(this).addClass("active");
+    },
+    function() {
+      $(this).removeClass("active");
+    }
+  );
+
+  //   左边  导航栏
+  $(".navinner-ul .navinner-li").hover(
+    function() {
+      $(".fenleinav").slideDown("500", function() {
+        $(".fenleinav").show();
+      });
+    },
+    function() {
+      $(".fenleinav").slideUp("500"),
+        function() {
+          $(".fenleinav").hide();
+        };
+    }
+  );
+
+  $(".fenleinav .p").hover(
+    function() {
+      $this = $(this);
+      $(this).addClass("active").find('.three').show();
+      $(this).siblings().find('.three').hide();
+    },
+    function() {
+      $(this).removeClass("active").find('.three').hide();
+    }
+  );
+});
